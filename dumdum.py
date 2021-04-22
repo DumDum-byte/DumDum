8bfc96cf61c6a0b326a48dd6f04d445fc91a9e3e
DumDum-byte <82753412+DumDum-byte@users.noreply.github.com>
2021-04-22 12:56:26 +0530

    Create dumdum.py

---
+++ dumdum.py
@@ -0,0 +1,37 @@
+#config
+import discord
+import flask
+import os
+
+client = discord.Client()
+
+#login
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+#commands
+
+#hello
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("bruh hello"):
+        await message.channel.send("Hello, Im dumdum Nice to meet you Onee Chans")
+    
+    if message.content.startswith("bruh help"):
+        await message.channel.send("Hello, dumdum is currently devloping me wait a while")
+    
+
+
+
+
+
+
+
+
+
+
+client.run(os.getenv('secret'))
